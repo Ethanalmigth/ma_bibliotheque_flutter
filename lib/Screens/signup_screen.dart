30f30a14bfbf81427f9dd8279513424e2cbffa40
Jaures HOUNDJAHOUE <jaureshoundjahoue@gmail.com>
2024-05-24 14:25:41 +0100

    Nouvelle version derniere version de mon projet de memoire

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -17,12 +17,12 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> {
   final _auth = FirebaseAuth.instance;
+  final _formkey =GlobalKey<FormState>();
   late String _name;
   late String _firstname;
   late String _email;
   late String _password;
   late String _confirmPass;
-
   bool _saving = false;
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _firstnameController = TextEditingController();
@@ -52,143 +52,148 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       padding: const EdgeInsets.symmetric(
                         horizontal: 15,
                       ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const ScreenTitle(title: 'Sign Up'),
+                      child: Form(
+                        key: _formkey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const ScreenTitle(title: 'Sign Up'),
 
-                          CustomTextField(
-                            textField: TextField(
-                              controller: _nameController,
-                              onChanged: (value) {
-                                _name = value;
-                              },
-                              style: const TextStyle(
-                                fontSize: 20,
+                            CustomTextField(
+                              textField: TextField(
+                                controller: _nameController,
+                                onChanged: (value) {
+                                  _name = value;
+                                },
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                ),
+                                decoration: kTextInputDecoration.copyWith(
+                                  hintText: 'Nom',
+                                ),
                               ),
-                              decoration: kTextInputDecoration.copyWith(
-                                hintText: 'Nom',
-                              ),
+
                             ),
 
-                          ),
+                            CustomTextField(
+                              textField: TextField(
+                                controller: _firstnameController,
+                                onChanged: (value) {
+                                  _firstname = value;
+                                },
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                ),
+                                decoration: kTextInputDecoration.copyWith(
+                                  hintText: 'Prenom',
+                                ),
+                              ),
+                            ),
+                            CustomTextField(
+                              textField: TextField(
+                                controller: _emailController,
+                                onChanged: (value) {
+                                  _email = value;
+                                },
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                ),
+                                decoration: kTextInputDecoration.copyWith(
+                                  hintText: 'Email',
+                                ),
+                              ),
+                            ),
+                            CustomTextField(
+                              textField: TextField(
+                                controller: _passwordController,
+                                obscureText: true,
+                                onChanged: (value) {
+                                  _password = value;
+                                },
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                ),
+                                decoration: kTextInputDecoration.copyWith(
+                                  hintText: 'Password',
+                                ),
+                              ),
+                            ),
+                            CustomTextField(
+                              textField: TextField(
+                                controller: _confirmPassController,
+                                obscureText: true,
+                                onChanged: (value) {
+                                  _confirmPass = value;
+                                },
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                ),
+                                decoration: kTextInputDecoration.copyWith(
+                                  hintText: 'Confirm Password',
+                                ),
+                              ),
+                            ),
+                            CustomBottomScreen(
+                              textButton: 'Sign Up',
+                              heroTag: 'signup_btn',
+                              question: 'Have an account?',
+                              buttonPressed: () async {
 
-                          CustomTextField(
-                            textField: TextField(
-                              controller: _firstnameController,
-                              onChanged: (value) {
-                                _firstname = value;
-                              },
-                              style: const TextStyle(
-                                fontSize: 20,
-                              ),
-                              decoration: kTextInputDecoration.copyWith(
-                                hintText: 'Prenom',
-                              ),
-                            ),
-                          ),
-                          CustomTextField(
-                            textField: TextField(
-                              controller: _emailController,
-                              onChanged: (value) {
-                                _email = value;
-                              },
-                              style: const TextStyle(
-                                fontSize: 20,
-                              ),
-                              decoration: kTextInputDecoration.copyWith(
-                                hintText: 'Email',
-                              ),
-                            ),
-                          ),
-                          CustomTextField(
-                            textField: TextField(
-                              controller: _passwordController,
-                              obscureText: true,
-                              onChanged: (value) {
-                                _password = value;
-                              },
-                              style: const TextStyle(
-                                fontSize: 20,
-                              ),
-                              decoration: kTextInputDecoration.copyWith(
-                                hintText: 'Password',
-                              ),
-                            ),
-                          ),
-                          CustomTextField(
-                            textField: TextField(
-                              controller: _confirmPassController,
-                              obscureText: true,
-                              onChanged: (value) {
-                                _confirmPass = value;
-                              },
-                              style: const TextStyle(
-                                fontSize: 20,
-                              ),
-                              decoration: kTextInputDecoration.copyWith(
-                                hintText: 'Confirm Password',
-                              ),
-                            ),
-                          ),
-                          CustomBottomScreen(
-                            textButton: 'Sign Up',
-                            heroTag: 'signup_btn',
-                            question: 'Have an account?',
-                            buttonPressed: () async {
-                              FocusManager.instance.primaryFocus?.unfocus();
-                              setState(() {
-                                _saving = true;
-                              });
-                              if (_confirmPass == _password) {
-                                try {
-                                  await _auth.createUserWithEmailAndPassword(
-                                      email: _email, password: _password);
+                                /*
+                                FocusManager.instance.primaryFocus?.unfocus();
+                                setState(() {
+                                  _saving = true;
+                                });
+                                if (_confirmPass == _password) {
+                                  try {
+                                    await _auth.createUserWithEmailAndPassword(
+                                        email: _email, password: _password);
 
-                                  if (context.mounted) {
+                                    if (context.mounted) {
+                                      signUpAlert(
+                                        context: context,
+                                        title: 'GOOD JOB',
+                                        desc: 'Go login now',
+                                        btnText: 'Login Now',
+                                        onPressed: () {
+                                          setState(() {
+                                            _saving = false;
+                                            Navigator.popAndPushNamed(
+                                                context, SignUpScreen.id);
+                                          });
+                                          Navigator.pushNamed(
+                                              context, LoginScreen.id);
+                                        },
+                                      ).show();
+                                    }
+                                  } catch (e) {
                                     signUpAlert(
-                                      context: context,
-                                      title: 'GOOD JOB',
-                                      desc: 'Go login now',
-                                      btnText: 'Login Now',
-                                      onPressed: () {
-                                        setState(() {
-                                          _saving = false;
-                                          Navigator.popAndPushNamed(
-                                              context, SignUpScreen.id);
-                                        });
-                                        Navigator.pushNamed(
-                                            context, LoginScreen.id);
-                                      },
-                                    ).show();
+                                        context: context,
+                                        onPressed: () {
+                                          SystemNavigator.pop();
+                                        },
+                                        title: 'SOMETHING WRONG',
+                                        desc: 'Close the app and try again',
+                                        btnText: 'Close Now');
                                   }
-                                } catch (e) {
-                                  signUpAlert(
+                                } else {
+                                  showAlert(
                                       context: context,
+                                      title: 'WRONG PASSWORD',
+                                      desc:
+                                      'Make sure that you write the same password twice',
                                       onPressed: () {
-                                        SystemNavigator.pop();
-                                      },
-                                      title: 'SOMETHING WRONG',
-                                      desc: 'Close the app and try again',
-                                      btnText: 'Close Now');
-                                }
-                              } else {
-                                showAlert(
-                                    context: context,
-                                    title: 'WRONG PASSWORD',
-                                    desc:
-                                    'Make sure that you write the same password twice',
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    }).show();
-                              }
-                            },
-                            questionPressed: () async {
-                              Navigator.pushNamed(context, LoginScreen.id);
-                            },
-                          ),
-                        ],
+                                        Navigator.pop(context);
+                                      }).show();
+                                }*/
+                              },
+                              questionPressed: () async {
+                                Navigator.pushNamed(context, LoginScreen.id);
+                              },
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),

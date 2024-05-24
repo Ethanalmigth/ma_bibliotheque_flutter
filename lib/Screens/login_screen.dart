9d954092fe37d0bd7e7db575bdf50f4daf7bd78c
Jaures HOUNDJAHOUE <jaureshoundjahoue@gmail.com>
2024-05-24 16:01:59 +0100

    Derniere version de 24/02/24

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -16,9 +16,13 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
+  final _formkey =GlobalKey<FormState>();
   late String _email;
   late String _password;
   bool _saving = false;
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -40,95 +44,109 @@ class _LoginScreenState extends State<LoginScreen> {
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 children: [
-                  const TopScreenImage(screenImageName: 'welcome.png'),
                   Expanded(
                     flex: 2,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const ScreenTitle(title: 'Login'),
-                        CustomTextField(
-                          hintText: 'Email',
-                          onChanged: (value) {
-                            _email = value;
-                          },
-                        ),
-
-                        CustomTextField(
-                          hintText: 'Password',
-                          onChanged: (value) {
-                            _email = value;
-                          },
-                        ),
-                        const Text(
-                          'Connecte toi avec ton compte google',
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16,
+                    child: Form(
+                      key: _formkey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const ScreenTitle(title: 'Login'),
+                          CustomTextField(
+                            hintText: 'Email',
+                            controller: _emailController,
+                            onChanged: (value) {
+                              _email = value;
+                            },
                           ),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: CircleAvatar(
-                            radius: 25,
-                            child: Image.asset(
-                                'asset/images/logo.png'),
-                          ),
-                        ),
-                        CustomBottomScreen(
-                          textButton: 'Login',
-                          heroTag: 'login_btn',
-                          question: 'Forgot password?',
-                          buttonPressed: () async {
-                            FocusManager.instance.primaryFocus?.unfocus();
-                            setState(() {
-                              _saving = true;
-                            });
-                            try {
-                              await _auth.signInWithEmailAndPassword(
-                                  email: _email, password: _password);
 
-                              if (context.mounted) {
-                                setState(() {
-                                  _saving = false;
-                                  Navigator.popAndPushNamed(
-                                      context, LoginScreen.id);
-                                });
-                                Navigator.pushNamed(context, WelcomeScreen.id);
-                              }
-                            } catch (e) {
-                              signUpAlert(
-                                context: context,
-                                onPressed: () {
+                          CustomTextField(
+                            hintText: 'Password',
+                            controller: _passwordController,
+                            onChanged: (value) {
+                              _email = value;
+                            },
+                          ),
+                          const Text(
+                            'Connecte toi avec ton compte google',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: CircleAvatar(
+                              radius: 25,
+                              child: Image.asset(
+                                  'asset/images/logo.png'),
+                            ),
+                          ),
+                          CustomBottomScreen(
+                            textButton: 'Login',
+                            heroTag: 'login_btn',
+                            question: 'Forgot password?',
+                            buttonPressed: () async {
+                                  if(_formkey.currentState!.validate()){
+                                    final email= _emailController.text;
+                                    final pass= _passwordController.text;
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        const SnackBar(content: Text("Envoie en cours..."))
+                                    );
+                                    FocusScope.of(context).requestFocus(FocusNode());
+                                    print("Son est email $email et son mot de passe est $pass ");
+                                  }
+                              /*
+                              FocusManager.instance.primaryFocus?.unfocus();
+                              setState(() {
+                                _saving = true;
+                              });
+                              try {
+                                await _auth.signInWithEmailAndPassword(
+                                    email: _email, password: _password);
+
+                                if (context.mounted) {
                                   setState(() {
                                     _saving = false;
+                                    Navigator.popAndPushNamed(
+                                        context, LoginScreen.id);
                                   });
-                                  Navigator.popAndPushNamed(
-                                      context, LoginScreen.id);
+                                  Navigator.pushNamed(context, WelcomeScreen.id);
+                                }
+                              } catch (e) {
+                                signUpAlert(
+                                  context: context,
+                                  onPressed: () {
+                                    setState(() {
+                                      _saving = false;
+                                    });
+                                    Navigator.popAndPushNamed(
+                                        context, LoginScreen.id);
+                                  },
+                                  title: 'WRONG PASSWORD OR EMAIL',
+                                  desc:
+                                  'Confirm your email and password and try again',
+                                  btnText: 'Try Now',
+                                ).show();
+                              }*/
+                            },
+                            questionPressed: () {
+                              signUpAlert(
+                                onPressed: () async {
+                                  await FirebaseAuth.instance
+                                      .sendPasswordResetEmail(email: _email);
                                 },
-                                title: 'WRONG PASSWORD OR EMAIL',
+                                title: 'RESET YOUR PASSWORD',
                                 desc:
-                                'Confirm your email and password and try again',
-                                btnText: 'Try Now',
+                                'Click on the button to reset your password',
+                                btnText: 'Reset Now',
+                                context: context,
                               ).show();
-                            }
-                          },
-                          questionPressed: () {
-                            signUpAlert(
-                              onPressed: () async {
-                                await FirebaseAuth.instance
-                                    .sendPasswordResetEmail(email: _email);
-                              },
-                              title: 'RESET YOUR PASSWORD',
-                              desc:
-                              'Click on the button to reset your password',
-                              btnText: 'Reset Now',
-                              context: context,
-                            ).show();
-                          },
-                        ),
-                      ],
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
